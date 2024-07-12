dcedf76e3e9cee17b8f406a6c8cb8c8d811a81b0
Cindy <cindyakumu40@gmail.com>
2024-07-12 15:27:59 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function countDown(start) {
+    if (typeof start !== 'number' || start < 1) {
+      console.error('Please enter a positive integer.');
+      return;
+    }
+  
+    let currentNumber = start; 
+    while (currentNumber >= 0) {
+      console.log(currentNumber);
+      currentNumber--;
+    }
+  }
+// index.js
+
+function writeCards(names) {
+    const messages = names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+    return messages;
+  }
+  
+  // Example usage
+  const names = ['Guadalupe', 'Ollie', 'Aki'];
+  const thankYouMessages = writeCards(names);
+  
+  console.log(thankYouMessages);
+  
